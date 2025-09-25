963d42668206885c20d6f2d8fbc7926d6d8a75e0
Dhananjoy Bhuyan <dhananjoy.bhuyan.2022@gmail.com>
2025-09-26 02:03:29 +0530

    Add files via upload

--- pages.py
+++ pages.py
@@ -3,8 +3,37 @@
 """
 Created on Fri Sep 26 01:13:51 2025
 
-@author: dev
+@author: Dhananjoy Bhuyan
 """
 
 
-import streamlit as st
+import streamlit as st  # for UI/UX
+from streamlit import session_state as ss  # persistance of values
+from load_datasets import load_all_datasets, load_specific_dataset  # loaaaaad!!!
+from load_models import eager_load_all_models, lazy_load_model  # looooaaaad again!!
+
+
+if 'page' not in ss:
+    ss.page = 'show_data'
+
+
+def show_data():  # page 1
+    st.markdown("""
+                <h4>Choose a Dataset:</h4>
+                """, unsafe_allow_html=True)  # header
+    names = load_all_datasets(True)  # get the names.
+    choice = st.selectbox('datasets', names)  # take input.
+    if st.button('Show all datasets'):
+        for name, dset in load_all_datasets().items():
+            st.subheader(name)
+            st.dataframe(dset)
+    else:
+        st.dataframe(load_specific_dataset(choice))
+
+    if st.button('Upload my own data'):
+        pass
+
+
+if __name__ == "__main__":
+    if ss.page == "show_data":
+        show_data()
